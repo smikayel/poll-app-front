a6f710497cf94698e23845016cae33a92b0e90ed
smikayel <sasha-mikayelyan@mail.ru>
2022-09-28 18:07:11 +0400

    add the pagination and the poll active deactivation

--- user/middleware.py
+++ user/middleware.py
@@ -1,21 +1,49 @@
+from time import sleep
 import jwt
 from django.http import HttpResponsePermanentRedirect
 class CustomMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.AUTH_URL = '/user/auth'
+        self.NON_PROTECTED = ['/user/auth', '/register']
 
     def __call__(self, request):
-        try:
-            token = request.COOKIES["Authorization"]  
+        if request.path not in self.NON_PROTECTED:
             try:
+                token = request.COOKIES["Authorization"]
                 payload = jwt.decode(token, 'secret', algorithms=["HS256"])
-                if request.path == self.AUTH_URL:
-                   return  HttpResponsePermanentRedirect('/home')
+                return self.get_response(request)
             except:
-                if request.path == self.AUTH_URL:
-                   return self.get_response(request) 
-        except:
-            return HttpResponsePermanentRedirect('/user/auth')
-        return self.get_response(request)
+                return HttpResponsePermanentRedirect('/user/auth') 
+        else:
+            try:
+                token = request.COOKIES["Authorization"]
+                payload = jwt.decode(token, 'secret', algorithms=["HS256"])
+                flag = True
+            except:
+                flag = False
+            if not flag:
+                return self.get_response(request)
+            else:
+                return HttpResponsePermanentRedirect('/home') 
+
         
+        
+        
+        
+        
+        
+        
+        # flag = False
+        # try:
+        #     token = request.COOKIES["Authorization"]
+        #     payload = jwt.decode(token, 'secret', algorithms=["HS256"])
+        # except:
+        #     flag = True
+        # if request.path !=  self.AUTH_URL:
+        #     return self.get_response(request)
+        # else:
+        #     if flag:
+        #         return HttpResponsePermanentRedirect('/user/auth')
+        #     return HttpResponsePermanentRedirect('/home')
+        # except:
+        #     return HttpResponsePermanentRedirect('/user/auth')
